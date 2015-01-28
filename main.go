11ea4fdb071e30d3a51a9c3afbf03b06a4ec3789
Laura Frank <ljfrank@gmail.com>
2015-01-28 14:35:00 -0600

    Adding command line flags

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import (
 	"github.com/centurylinklabs/panamax-marathon-adapter/api"
 	"github.com/centurylinklabs/panamax-marathon-adapter/marathon"
 	"os"
+	"flag"
 )
 
 func main() {
-	var endpoint = ""
-	if endpoint = os.Getenv("MARATHON_ENDPOINT"); endpoint == "" {
+	//set up command line flag
+	var cmd_endpoint string
+	flag.StringVar(&cmd_endpoint, "endpoint", "", "Marathon Endpoint")
+
+	endpoint := os.Getenv("MARATHON_ENDPOINT");
+
+	if endpoint == "" {
+		flag.Parse();
+		endpoint = cmd_endpoint;
+	}
+
+	if  endpoint == "" {
 		fmt.Println("Error: Invalid endpoint url. Set env. var. 'MARATHON_ENDPOINT' correctly. ")
 		os.Exit(1)
 	}
